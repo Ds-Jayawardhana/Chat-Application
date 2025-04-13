58b96595474304893c38effcaebdfbd4c629a4dd
Disandu Sanhida Jayawardhane <disandu.20230469@iit.ac.lk>
2025-04-13 19:23:24 +0530

    Create project Structure for the Tute 8

---
+++ Completed/tute8ans/src/main/java/com/university/tute8ans/tute8ans/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.university.tute8ans.tute8ans;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
