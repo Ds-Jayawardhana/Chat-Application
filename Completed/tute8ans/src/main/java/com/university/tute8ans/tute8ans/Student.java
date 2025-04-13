bb4151785ec38dcefcf5b323e9cfb7e7bae6371c
Disandu Sanhida Jayawardhane <disandu.20230469@iit.ac.lk>
2025-04-13 21:11:51 +0530

    Updated Web xml File

---
+++ Completed/tute8ans/src/main/java/com/university/tute8ans/tute8ans/Student.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.university.tute8ans.tute8ans;
+
+/**
+ *
+ * @author user
+ */
+public class Student {
+    String id;
+    String firstName;
+    String lastName;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+
+    public Student() {
+    }
+
+    public Student(String id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    
+    
+}
